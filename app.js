df92ed8f7a90ef1a9f637da2bbb5dcde173e904d
Lucas M <montenoise.lucas@gmail.com>
2018-07-11 16:27:57 +0200

    Routes are ok, but need ensure login for admin users

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-const session    = require("express-session");
-const MongoStore = require('connect-mongo')(session);
-const cors       = require( "cors" );
-const flash = require( "connect-flash" );
-
+const bodyParser   = require( 'body-parser' );
+const cookieParser = require( 'cookie-parser' );
+const express      = require( 'express' );
+const favicon      = require( 'serve-favicon' );
+const hbs          = require( 'hbs' );
+const mongoose     = require( 'mongoose' );
+const session      = require( "express-session" );
+const passport     = require( "passport" );
+const MongoStore   = require( 'connect-mongo' )( session );
+const logger       = require( 'morgan' );
+const path         = require( 'path' );
+const cors         = require( "cors" );
+const flash        = require( "connect-flash" );
 
 
 
@@ -24,6 +24,10 @@ mongoose
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
+
+const passportSetup = require('./passport/index');
+passportSetup(passport);
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -50,6 +54,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use((req, res, next) => {
+  res.sendfile(__dirname + '/public/index.html');
+});
 
 
 
